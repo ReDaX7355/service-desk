e72b98efd2e4489e23a4f528e10a84482a073dba
Ruslan <43163998+ReDaX7355@users.noreply.github.com>
2024-03-25 12:33:33 +0000

    Кореектировать конфиг webpack

--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const tailwindcss = require('tailwindcss');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const tailwindcss = require("tailwindcss");
 
 const rules = [
   {
     test: /\.[jt]sx?$/i,
-    exclude: '/node_modules/',
+    exclude: "/node_modules/",
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
         cacheDirectory: true,
       },
@@ -19,12 +19,12 @@ const rules = [
     test: /\.(s[ac]|c)ss$/i,
     use: [
       MiniCssExtractPlugin.loader,
-      'css-loader',
+      "css-loader",
       {
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           postcssOptions: {
-            plugins: [['postcss-preset-env', {}], tailwindcss],
+            plugins: [["postcss-preset-env", {}], tailwindcss],
           },
         },
       },
@@ -32,21 +32,21 @@ const rules = [
   },
   {
     test: /\.(s[ac])ss$/i,
-    use: ['sass-loader'],
+    use: ["sass-loader"],
   },
   {
     test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-    type: 'asset/resource',
+    type: "asset/resource",
     generator: {
-      filename: 'assets/images/[hash][ext]',
+      filename: "assets/images/[hash][ext]",
     },
   },
   {
     test: /\.(woff2?|eot|ttf|otf)$/i,
-    exclude: '/node_modules/',
-    type: 'asset/resource',
+    exclude: "/node_modules/",
+    type: "asset/resource",
     generator: {
-      filename: 'assets/fonts/[hash][ext]',
+      filename: "assets/fonts/[hash][ext]",
     },
   },
 ];
@@ -54,26 +54,27 @@ const rules = [
 module.exports = {
   entry: {
     index: {
-      import: path.resolve(__dirname, './../src/index.tsx'),
+      import: path.resolve(__dirname, "./../src/index.tsx"),
     },
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   performance: {
     hints: false,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Caching',
+      title: "Caching",
       minify: false,
       hash: true,
-      template: path.resolve(__dirname, './../src/index.html'),
+      template: path.resolve(__dirname, "./../src/index.html"),
     }),
   ],
   devServer: {
     historyApiFallback: true,
+    allowedHosts: "all",
   },
   output: {
     clean: true,
